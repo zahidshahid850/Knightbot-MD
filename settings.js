b38054b1dd1faf6084c2eb4fdeb7e00c91fd421e
zahidshahid850 <shahidzahid691@gmail.com>
2025-04-06 11:22:53 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Shahid Bot', // Your name
+  ownerNumber: '923058808197', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
